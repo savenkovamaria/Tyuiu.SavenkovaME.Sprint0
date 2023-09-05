c4a2f0dc998110b1fb771da234bae613daa50c53
Tyuiu.SavenkovaME@outlook.com <savenkova_mars@mail.ru>
2023-09-05 21:12:53 +0500

    Task1

---
+++ Tyuiu.SavenkovaME.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.SavenkovaME.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Мария";
+            int age = 17;
+            Console.WriteLine("Здравствуйте, меня зовут " + name + " мне " + age + " лет. ");
+            Console.ReadKey();
+        }
+    }
+}
